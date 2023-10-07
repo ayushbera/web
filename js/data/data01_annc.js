2fbd179140f8106ba8339b55cde694c94b39f434
ayushbera <145838266+ayushbera@users.noreply.github.com>
2023-10-07 09:12:11 +0530

    Update data01_annc.js

--- js/data/data01_annc.js
+++ js/data/data01_annc.js
@@ -8,5 +8,10 @@ const data01 = [
   title: 'Test (Latest)',
   body: 'This is a <b> LATEST TEST</b> announcement.',
   date: '29, September 2023'
- } */
+ }, */
+ {
+  title: 'Website is now live!',
+  body: 'The website is now live at <a href="https://ayushbera.github.io/web/">https&#58;&#47;&#47;ayushbera.github.io&#47;web&#47;</a>',
+  date: '07, October 2023'
+ }
 ];

adf97ab561ede0c29070d2fd53037b9fd208c034
ayushbera <145838266+ayushbera@users.noreply.github.com>
2023-10-07 09:13:14 +0530

    Remove data00_stdns.js data

--- js/data/data00_stdns.js
+++ js/data/data00_stdns.js
@@ -1,12 +1,12 @@
 const data00 = [
- {
+ /* {
   name: "John Doe",
   exam: "CET"
  },
  {
   name: "Joe Blow",
   exam: "MS-CIT"
- },
+ }, */
  {
   name: "Ayush Bera",
   exam: "JEE (Mains & Advance)"
